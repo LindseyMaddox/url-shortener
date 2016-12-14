1b81b57c188ccaab1a6e54353ba63907cdd52f8f
Lindsey Maddox <llmaddox@gmail.com>
2016-12-14 15:39:40 +0000

    Initial work. Checking if valid url and returning json

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
+//if not a valid url, json will say error {"error":"This url is not on the database."}
+//when you enter (OR CLICK ON) your shortened url, it will redirect to original site
+
+
 var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-res.send("Please add url to end of root url to get your shortener");
+res.send("Please add url to end of root url to get your shortener. Example: https://shorten-your-url.herokuapp.com/new/https://google.com");
 });
 
-app.get('/', function(req, res){
-var original_url = req.url;
+app.get('/new/:site', function(req, res){
+  var valid_url_regex = "https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
+  var valid_site = req.params.site.match(/valid_url_regex/);
+  if(valid_site){
+    var original_url = req.params.site;
+    var parent_url = req.url;
+    var data = { "original_url": original_url, "parent_url": parent_url };
 
-res.send("Please add url to end of root url to get your shortener");
+  } else {
+    var data = { "error":"Wrong url format. Please check your url again." }
+  }
+
+res.send(JSON.stringify(data, null, 2));
 });
+
+app.get('/:shortened_url', function(req,res){
+  //find parent url by shortened url
+})
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
